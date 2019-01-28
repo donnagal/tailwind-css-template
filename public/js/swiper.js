0457e56198d61ed499b9567abecf49e5f0d99fa5
Donna Galletta <donna.galletta@isobar.com>
2019-01-29 10:06:45 +1100

    Fix slider

--- public/js/swiper.js
+++ public/js/swiper.js
@@ -42,22 +42,4 @@ var swiper = new Swiper('.swiper-container', {
   }
 })
 
-// if ($(window).width() < 760) {
-//   //Small
-//   var swiper = new Swiper('.swiper-container', {
-//     slidesPerView: 1.5,
-//     spaceBetween: 15,
-//     centeredSlides: true,
-//     pagination: '.swiper-pagination',
-//     paginationClickable: true,
-//   });
-// }
-// else {
-// //Large
-//   var swiper = new Swiper('.swiper-container', {
-//     slidesPerView: 3,
-//     spaceBetween: 20,
-//     preventInteractionOnTransition: true,
-//   });
-// }
 
