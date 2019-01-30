fa9e2ccc2747bf0824dbce6c365f0b39a1aa8de6
Donna Galletta <donna.galletta@isobar.com>
2019-01-30 16:30:48 +1100

    Update footer 2

--- public/js/main.js
+++ public/js/main.js
@@ -23,39 +23,6 @@ $(function() {
   $('.loading').fadeIn('fast').delay(1000).fadeOut('slow');
 
 
-  // Background Movement 
-    var lFollowX = 0,
-    lFollowY = 0,
-    a = 0,
-    y = 0,
-    friction = 1 / 10;
-
-    function moveBackground() {
-    a += (lFollowX - a) * friction;
-    y += (lFollowY - y) * friction;
-
-    translate = 'translate(' + a + 'px, ' + y + 'px) scale(1.1)';
-
-    $('.bg-move').css({
-    '-webit-transform': translate,
-    '-moz-transform': translate,
-    'transform': translate
-    });
-
-    window.requestAnimationFrame(moveBackground);
-    }
-
-    $(window).on('mousemove click', function(e) {
-
-    var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
-    var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
-    // lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
-    lFollowY = (10 * lMouseY) / 100;
-
-    });
-
-  moveBackground();
-
   //nav
   $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
@@ -154,39 +121,4 @@ $(function() {
 
 
 
-  //Request Animation Frame Hook
-//http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-
-(function(window) {
-
-  var prefixes = 'webkit moz ms o'.split(' ');
-  // get unprefixed rAF and cAF, if present
-  var requestAnimationFrame = window.requestAnimationFrame;
-  var cancelAnimationFrame = window.cancelAnimationFrame;
-  // loop through vendor prefixes and get prefixed rAF and cAF
-  var prefix;
-
-  for (var i = 0; i < prefixes.length; i++) {
-      if (requestAnimationFrame && cancelAnimationFrame) {
-          break;
-      }
-      prefix = prefixes[i];
-      requestAnimationFrame = requestAnimationFrame || window[prefix + 'RequestAnimationFrame'];
-      cancelAnimationFrame = cancelAnimationFrame || window[prefix + 'CancelAnimationFrame'] || window[prefix + 'CancelRequestAnimationFrame'];
-  }
-
-  if (!requestAnimationFrame || !cancelAnimationFrame) {
-      requestAnimationFrame = function(callback, element) {
-          window.setTimeout(callback, 1000 / 60);
-      }
-
-      cancelAnimationFrame = function(id) {
-          window.clearTimeout(id);
-      };
-  }
-  // put in global namespace
-  window.requestAnimationFrame = requestAnimationFrame;
-  window.cancelAnimationFrame = cancelAnimationFrame;
-
-
-})(window);
+  
